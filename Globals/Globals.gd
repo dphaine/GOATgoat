b151654c065bd6dadbfc7da9d66ea302d1f307de
Philip Haine <phaine@productvision.com>
2021-08-01 12:19:00 -0700

    Added back goat state enum and fixed issue with global

--- Globals/Globals.gd
+++ Globals/Globals.gd
@@ -5,21 +5,22 @@ extends Node2D
 # Since this is a singleton, these are globals and can be 
 # shared between scenes
 
-export var startOfTunnelLocation
+# Access this with: Globals.startOfTunnelLocation = $StartOfTunnel
+var startOfTunnelLocation : Position2D
 
-# Paths to subsequent levels.  The last one is the final tally
-const levelPaths = ["res://Level1.tscn", "res://Level2.tscn", "res://Level3.tscn", "res://fullWinScreen.tscn"]
-const menuScreenPath : String = "res://title_screen/TitleScreen.tscn"
-const caughtScreenPath : String = "res://CaughtScreen.tscn"
-const winScreenPath : String = "res://WinScreen.tscn"
+## Paths to subsequent levels.  The last one is the final tally
+#const levelPaths = ["res://Level1.tscn", "res://Level2.tscn", "res://Level3.tscn", "res://fullWinScreen.tscn"]
+#const menuScreenPath : String = "res://title_screen/TitleScreen.tscn"
+#const caughtScreenPath : String = "res://CaughtScreen.tscn"
+#const winScreenPath : String = "res://WinScreen.tscn"
 
 # Is the player big or small?
-var playerIsBig : bool = false  # Access using Globals.playerIsBig
-var playerPosition : Vector2 # So net can find the fish
+#var playerIsBig : bool = false  # Access using Globals.playerIsBig
+#var playerPosition : Vector2 # So net can find the fish
 
-var stopwatchTime : float = 0.0 # current stopwatch time
-var level : int = 1 # The current level you are on
-var levelTimes = [0.0, 0.0, 0.0] # Times of all three levels
+#var stopwatchTime : float = 0.0 # current stopwatch time
+#var level : int = 1 # The current level you are on
+#var levelTimes = [0.0, 0.0, 0.0] # Times of all three levels
 
 #var currentLevel = 0 setLevel
 #var score = 0 setget setScore
@@ -34,7 +35,8 @@ var levelTimes = [0.0, 0.0, 0.0] # Times of all three levels
 func _ready():
 	#Sound.play("res://sounds/Chamber-of-Jewels.mp3", -11)
 #	$ThemeMusicPlayer.play()
-	MusicPlayerGlobal.playIt()
+#	MusicPlayerGlobal.playIt()
+	pass
 
 # When score is changed
 #func setScore(newScore):
--- goat/goat.gd
+++ goat/goat.gd
@@ -2,14 +2,31 @@ extends RigidBody2D
 
 var target:Position2D = null
 
+enum goatState {
+	BABY_GOING_TO_PASTURE,
+	BABY_GROWING,
+	MILLING_IN_PASTURE,
+	GOING_TO_TUNNEL,
+	IN_TUNNEL,
+	IN_UNKNOWN_DESTINY,
+	IN_BREEDING_AREA,
+	BREEDING,
+	RETIRING
+}
+
+var goatMindset = goatState.MILLING_IN_PASTURE
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	linear_velocity = Vector2(50, 20)
 	add_to_group("goats")
 
-
 var speed = 250
-
-	
 var hue = 0.0
+
+func _process(_delta):
+	if goatMindset == goatState.MILLING_IN_PASTURE:
+		pass
+	elif goatMindset == goatState.GOING_TO_TUNNEL:
+		pass
 

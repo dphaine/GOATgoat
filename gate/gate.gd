b065386cf337e65197b8976179336a65caec8612
Philip Haine <phaine@productvision.com>
2021-08-01 17:34:07 -0700

    Got the gate to work!

--- gate/gate.gd
+++ gate/gate.gd
@@ -1,15 +1,36 @@
 # Gate code
-extends StaticBody2D
+extends KinematicBody2D
 
 export var gateIsUp = false
 
+var gateAngle = 0.0
+var targetAngle = 0.0
+
+const GATE_SPEED = 400
+const GATE_ANGLE_DEGREES = 40 # How much should it go in one direction?
+const TOLERANCE = 3
+
+func _ready():
+	gateAngle = get_global_rotation_degrees()
+	targetAngle = gateAngle
+	print("Gate started at ", gateAngle)
+
 func setGate(isUp:bool) -> void:
+	print("changing gate to ", isUp)
 	gateIsUp = isUp
 	if gateIsUp:
-		set_global_rotation_degrees(35)
+		targetAngle = GATE_ANGLE_DEGREES
 	else:
-		set_global_rotation_degrees(-35)
+		targetAngle = -GATE_ANGLE_DEGREES
 
+func _process(delta):
+	var currentAngle = get_global_rotation_degrees()
+	var newAngle = currentAngle
+	if currentAngle < targetAngle - TOLERANCE:
+		newAngle += GATE_SPEED * delta
+	elif currentAngle > targetAngle + TOLERANCE:
+		newAngle -= GATE_SPEED * delta
+	set_global_rotation_degrees(newAngle)
 
 func _input(event):
 	if event.is_action_pressed("ui_up"):
